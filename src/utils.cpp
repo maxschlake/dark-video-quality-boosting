fd10eb48788de1fd7607e8c05a570f938af7e64c
maxschlake <mschlake@gmx.net>
2024-10-18 18:03:11 +0200

    fixed relative path

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,15 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <filesystem>
+
 
 int main()
 {
-    std::string imagePath = "C:/Users/maxim/Documents/Projects/video-quality-boosting/images/penguin.jpg"; // Absolute path
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
+    std::string imagePath = "images/penguin.jpg";
+    std::cout << "Current path: " << std::filesystem::current_path() << "\n";
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
     cv::imshow("Display window", image);
     cv::waitKey(0);
