8c046702e23a6489e51bfec91befd572019c27af
maxschlake <mschlake@gmx.net>
2024-10-23 16:49:20 +0200

    finalized saveImage() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <QApplication>
 #include "ReadImageQt.h"
+#include "WriteImage.h"
+#include <opencv2/opencv.hpp>
 
 int main (int argc, char *argv[])
 {
+    // READING
+    /*
     QApplication app(argc, argv);
-
     ReadImageQt readImageQt;
     readImageQt.showImage("images/raw/penguin.jpg");
     readImageQt.show();
-
     return app.exec();
+    */
+
+    //WRITING
+    cv::Mat image = cv::imread("images/raw/penguin.jpg");
+    WriteImage writer;
+    bool success = writer.saveImage(image, "images/mod/penguin.jpg");
+    return 0;
+    
 }
